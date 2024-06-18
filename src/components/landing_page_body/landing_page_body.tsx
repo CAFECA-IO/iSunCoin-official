0fa46c9a0eb78edfa3bc2530eba008156fff495c
Liz <mingreal75@gmail.com>
2024-06-18 18:15:30 +0800

    hero section kv of landing page

--- src/components/landing_page_body/landing_page_body.tsx
+++ src/components/landing_page_body/landing_page_body.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HeroSectionOfLandingPage from '@/components/hero_section_of_landing_page/hero_section_of_landing_page';
 
 const LandingPageBody = () => {
   return (
     <main className="mt-88px min-h-screen max-w-full overflow-x-hidden bg-surface-neutral-surface-lv1 font-barlow">
-      <h1 className="text-text-brand-primary-lv2">內文</h1>
+      <HeroSectionOfLandingPage />
     </main>
   );
 };
