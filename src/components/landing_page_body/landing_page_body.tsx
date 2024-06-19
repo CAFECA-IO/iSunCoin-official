a961a69748412445ea215c435d3fe41fd19a7f05
Liz <mingreal75@gmail.com>
2024-06-19 12:03:58 +0800

    introduction

--- src/components/landing_page_body/landing_page_body.tsx
+++ src/components/landing_page_body/landing_page_body.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import HeroSectionOfLandingPage from '@/components/hero_section_of_landing_page/hero_section_of_landing_page';
+import Introduction from '@/components/introduction/introduction';
 
 const LandingPageBody = () => {
   return (
     <main className="mt-88px min-h-screen max-w-full overflow-x-hidden bg-surface-neutral-surface-lv1 font-barlow">
       <HeroSectionOfLandingPage />
+
+      <Introduction />
     </main>
   );
 };
