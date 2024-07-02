5ea3f7a769849bc576c028836f37ac8bdb3d2601
Liz <mingreal75@gmail.com>
2024-07-02 14:50:36 +0800

    update ColorTypeSelector layout

--- src/components/three_models/color_type_selector.tsx
+++ src/components/three_models/color_type_selector.tsx
@@ -33,75 +33,73 @@ const ColorTypeSelector = () => {
   };
 
   return (
-    <section className="mx-auto w-full max-w-1440px p-20px">
-      <section className="flex items-start justify-center gap-80px">
-        {/* Text */}
-        <div className="space-y-40px">
-          <h1 className="text-64px font-bold text-text-neutral-primary">
-            Show your
-            <span className="text-surface-brand-primary"> Color</span>,
-            <br />
-            Select My
-            <span className="text-surface-brand-primary"> Type</span>.
-          </h1>
-          <p className="text-lg font-semibold text-text-neutral-primary">
-            Choose your preferred color, and it will be displayed in real-time on the device model
-            to the right, providing you with a color reference.
-          </p>
+    <section className="mx-auto flex max-w-1440px items-center justify-center py-40px">
+      {/* Text */}
+      <div className="flex-1 space-y-40px px-20px">
+        <h1 className="text-64px font-bold text-text-neutral-primary">
+          Show your
+          <span className="text-surface-brand-primary"> Color</span>,
+          <br />
+          Select My
+          <span className="text-surface-brand-primary"> Type</span>.
+        </h1>
+        <p className="text-lg font-semibold text-text-neutral-primary">
+          Choose your preferred color, and it will be displayed in real-time on the device model to
+          the right, providing you with a color reference.
+        </p>
 
-          {/* Button - choose type */}
-          <div className="flex items-center gap-20px">
-            <h6 className="text-xl font-bold text-surface-neutral-invert">Variation</h6>
-            <div className="flex gap-24px">
-              {types.map((type) => (
-                <button
-                  key={type}
-                  type="button"
-                  className={`rounded-xs border border-button-stroke-secondary-hover px-24px py-10px ${selectedType === type ? 'bg-button-surface-strong-secondary text-button-text-invert' : 'text-button-stroke-secondary-hover'}`}
-                  onClick={() => {
-                    setSelectedType(type);
-                    // setSelectedColor(null); // Reset selected color when type changes
-                  }}
-                >
-                  {type}
-                </button>
-              ))}
-            </div>
-          </div>
-
-          {/* Button - choose color */}
-          <div className="space-y-22px">
-            <h6 className="text-xl font-bold text-surface-neutral-invert">Type</h6>
-            <div className="flex gap-20px">
-              {Object.entries(colors).map(([color, className]) => (
-                <button
-                  key={color}
-                  type="button"
-                  className={`h-40px w-40px rounded-full shadow-downDropShadowXS ${className} ${selectedColor === color ? 'ring-4 ring-stroke-brand-primary' : ''}`}
-                  onClick={() => setSelectedColor(color)}
-                ></button>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        {/* Slider */}
-        <div className="max-w-1/2">
-          <Slider {...settings}>
-            {getImages(selectedType, selectedColor).map((image) => (
-              <div key={image}>
-                <Image
-                  src={image}
-                  alt={`slide-${image}`}
-                  width={520}
-                  height={520}
-                  className="h-auto w-full object-contain"
-                />
-              </div>
+        {/* Button - choose type */}
+        <div className="flex items-center gap-20px">
+          <h6 className="text-xl font-bold text-surface-neutral-invert">Variation</h6>
+          <div className="flex gap-24px">
+            {types.map((type) => (
+              <button
+                key={type}
+                type="button"
+                className={`rounded-xs border border-button-stroke-secondary-hover px-24px py-10px ${selectedType === type ? 'bg-button-surface-strong-secondary text-button-text-invert' : 'text-button-stroke-secondary-hover'}`}
+                onClick={() => {
+                  setSelectedType(type);
+                  // setSelectedColor(null); // Reset selected color when type changes
+                }}
+              >
+                {type}
+              </button>
             ))}
-          </Slider>
+          </div>
         </div>
-      </section>
+
+        {/* Button - choose color */}
+        <div className="space-y-22px">
+          <h6 className="text-xl font-bold text-surface-neutral-invert">Type</h6>
+          <div className="flex gap-20px">
+            {Object.entries(colors).map(([color, className]) => (
+              <button
+                key={color}
+                type="button"
+                className={`h-40px w-40px rounded-full shadow-downDropShadowXS ${className} ${selectedColor === color ? 'ring-4 ring-stroke-brand-primary' : ''}`}
+                onClick={() => setSelectedColor(color)}
+              ></button>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Slider */}
+      <div className="w-1/2 flex-1">
+        <Slider {...settings}>
+          {getImages(selectedType, selectedColor).map((image) => (
+            <div key={image}>
+              <Image
+                src={image}
+                alt={`slide-${image}`}
+                width={520}
+                height={520}
+                className="h-auto w-full object-cover"
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </section>
   );
 };
