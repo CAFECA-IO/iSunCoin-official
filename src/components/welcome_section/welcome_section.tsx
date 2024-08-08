a942530f1b3629d6685d48517d3d990ce7ea4125
julian <julian890701@gmail.com>
2024-08-08 16:26:28 +0800

    Feat: translate welcome section

--- src/components/welcome_section/welcome_section.tsx
+++ src/components/welcome_section/welcome_section.tsx
@@ -7,27 +7,23 @@ const WelcomeSection = () => {
   const introData = [
     {
       imageStr: '/elements/open_and_free.png',
-      title: 'Open and Free',
-      description:
-        'iSunCoin is an open and free internet technology that connects all computers to redefine the standard unit of computing power and provides an operational environment for tens of thousands of decentralized applications.',
+      title: t('HOME_PAGE.INTRO_CARD_1_TITLE'),
+      description: t('HOME_PAGE.INTRO_CARD_1_DESCRIPTION'),
     },
     {
       imageStr: '/elements/shared_computing.png',
-      title: 'Shared Computing Power',
-      description:
-        'iSunCoin connects all computers to redefine the standard unit of computing power and provides an operational environment for tens of thousands of decentralized applications.',
+      title: t('HOME_PAGE.INTRO_CARD_2_TITLE'),
+      description: t('HOME_PAGE.INTRO_CARD_2_DESCRIPTION'),
     },
     {
       imageStr: '/elements/privacy_protection.png',
-      title: 'Privacy Protection',
-      description:
-        'iSunCoin implements an accounting vector standard with zero-knowledge proof, allowing public information exchange in a standardized and privacy-protected format.',
+      title: t('HOME_PAGE.INTRO_CARD_3_TITLE'),
+      description: t('HOME_PAGE.INTRO_CARD_3_DESCRIPTION'),
     },
     {
       imageStr: '/elements/connecting_world.png',
-      title: 'Connecting the World',
-      description:
-        'iSunCoin can establish a globally accessible public ledger, easily creating application services for the entire world.',
+      title: t('HOME_PAGE.INTRO_CARD_4_TITLE'),
+      description: t('HOME_PAGE.INTRO_CARD_4_DESCRIPTION'),
     },
   ];
 
@@ -64,41 +60,27 @@ const WelcomeSection = () => {
         <div className="flex flex-col items-center gap-24px">
           {/* Info:(20240808 - Julian) Title */}
           <div className="flex flex-col items-center">
-            <p className="text-xl font-bold text-text-brand-primary-lv2">Welcome to</p>
+            <p className="text-xl font-bold text-text-brand-primary-lv2">
+              {t('HOME_PAGE.WELCOME')}
+            </p>
             <h1 className="text-64px font-semibold text-text-brand-secondary-lv1">iSunCoin</h1>
           </div>
           {/* Info:(20240808 - Julian) White Paper */}
-
           <a
             id="white-paper-btn"
             href={t('HOME_PAGE.WHITEPAPER_LINK')}
             download
             className="flex cursor-pointer items-center gap-4px rounded-full bg-button-surface-strong-secondary px-16px py-8px text-sm text-button-text-invert hover:bg-button-surface-strong-secondary-hover"
           >
-            <p>White Paper</p>
+            <p>{t('HOME_PAGE.WHITEPAPER')}</p>
             <Image src="/icons/book_open.svg" width={16} height={16} alt="book_icon" />
           </a>
           {/* Info:(20240808 - Julian) Description */}
           <div className="text-base text-text-neutral-primary">
-            <p>
-              iSunCoin is an open and free internet technology that connects all computers to
-              redefine the standard unit of computing power and provides an operational environment
-              for tens of thousands of decentralized applications.
-            </p>
-            <p>
-              It implements an accounting vector standard with zero-knowledge proof, allowing people
-              to publicly exchange information in a standardized and privacy-protected format. It
-              can establish a globally accessible public ledger and also enable people to create and
-              run decentralized application services on the platform.
-            </p>
-            <p>
-              The distributed ledger technology of iSunCoin is highly flexible, allowing users to
-              create any form of program logic, realizing systems we have yet to imagine.
-            </p>
-            <p>
-              Any computer can run iSunCoin, providing computing power to the platform and earning
-              rewards.
-            </p>
+            <p>{t('HOME_PAGE.WELCOME_DESCRIPTION_1')}</p>
+            <p>{t('HOME_PAGE.WELCOME_DESCRIPTION_2')}</p>
+            <p>{t('HOME_PAGE.WELCOME_DESCRIPTION_3')}</p>
+            <p>{t('HOME_PAGE.WELCOME_DESCRIPTION_4')}</p>
           </div>
         </div>
         {/* Info:(20240808 - Julian) Intro cards */}
