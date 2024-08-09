aeb90c1de7e0297019b44c206d70d870e0966877
Liz <mingreal75@gmail.com>
2024-08-09 15:22:22 +0800

    AutonomyPage: DecentralizedGovernance

--- src/components/autonomy_page/autonomy_page_body.tsx
+++ src/components/autonomy_page/autonomy_page_body.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
+import DecentralizedGovernance from '@/components/autonomy_page/decentralized_governance';
+import { useTranslation } from 'next-i18next';
 
 const AutonomyPageBody = () => {
+  const { t } = useTranslation('common');
+
   return (
     <main className="mt-88px min-h-screen max-w-full overflow-x-hidden bg-surface-neutral-main-background font-barlow">
       {/* HeroForAutonomyPage */}
@@ -16,18 +20,15 @@ const AutonomyPageBody = () => {
       {/* AutonomyBanner */}
       <section className="space-y-24px p-80px">
         <h1 className="text-center text-64px font-semibold text-text-brand-secondary-lv1">
-          iSunCoin Autonomy
+          {t('AUTONOMY_PAGE.AUTONOMY_TITLE')}
         </h1>
         <p className="text-base font-normal text-text-neutral-primary">
-          iSunCoin adopts decentralized governance, allowing coin holders to participate in
-          community governance through proposals and voting. Users holding iSunCoin automatically
-          become community members, with the right to propose improvements to the iSunCoin
-          ecosystem. After extensive community discussion and voting, decisions are written into
-          smart contracts and executed automatically through community consensus-driven
-          decision-making. This ensures the long-term healthy development of the project and
-          attracts more users and developers to join.
+          {t('AUTONOMY_PAGE.AUTONOMY_DESCRIPTION')}
         </p>
       </section>
+
+      {/* DecentralizedGovernance */}
+      <DecentralizedGovernance />
     </main>
   );
 };
