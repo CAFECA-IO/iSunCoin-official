6f5709e637c6a62be320fd3c558ea1779425d672
Liz <mingreal75@gmail.com>
2024-08-12 11:09:23 +0800

    i18n for use / develop page

--- src/components/develop_page/develop_page_body.tsx
+++ src/components/develop_page/develop_page_body.tsx
@@ -1,4 +1,9 @@
+import { useTranslation } from 'next-i18next';
+
 const DevelopPageBody = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { t } = useTranslation('common');
+
   return (
     <main className="mt-88px min-h-screen max-w-full overflow-x-hidden bg-surface-neutral-main-background font-barlow">
       <h1>Develop Page</h1>
--- src/pages/develop.tsx
+++ src/pages/develop.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { ILocale } from '@/interfaces/locale';
 import Header from '@/components/common/Header';
 import DevelopPageBody from '@/components/develop_page/develop_page_body';
 import Footer from '@/components/common/Footer';
@@ -23,5 +25,13 @@ const DevelopPage = () => {
     </>
   );
 };
+
+const getStaticPropsFunction = async ({ locale }: ILocale) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
+});
+
+export const getStaticProps = getStaticPropsFunction;
 
 export default DevelopPage;
