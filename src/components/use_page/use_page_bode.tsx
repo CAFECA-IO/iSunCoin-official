29fcdb9b54235002f6d149979fdacd28d52eb764
Liz <mingreal75@gmail.com>
2024-08-14 11:55:18 +0800

    UsePage: HeroForUsePage

--- src/components/use_page/use_page_bode.tsx
+++ src/components/use_page/use_page_bode.tsx
@@ -1,12 +1,38 @@
+import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 
 const UsePageBody = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { t } = useTranslation('common');
 
   return (
     <main className="mt-88px min-h-screen max-w-full overflow-x-hidden bg-surface-neutral-main-background font-barlow">
-      <h1>UsePageBody</h1>
+      {/* ===== HeroForUsePage ===== */}
+      <section className="flex items-center justify-between gap-20px bg-surface-brand-primary-10 pr-10px pt-32px xl:pr-80px">
+        {/* cover image */}
+        <div className="w-3/5">
+          <Image
+            src="/elements/hero_use_page.svg"
+            alt="hero_use_page"
+            width={932}
+            height={507}
+            className="-ml-16px"
+          ></Image>
+        </div>
+
+        {/* content */}
+        <div className="flex w-2/5 flex-col justify-center gap-24px">
+          <h1 className="text-44px font-bold text-text-neutral-primary">
+            {t('USE_PAGE.USING_ISUNCOIN')}
+          </h1>
+          <p className="text-base font-medium text-text-neutral-secondary">
+            {t('USE_PAGE.USING_ISUNCOIN_DESCRIPTION')}
+          </p>
+        </div>
+      </section>
+
+      {/* ===== WhereToUseCoin ===== */}
+
+      {/* ===== GiveMeCoin ===== */}
     </main>
   );
 };
