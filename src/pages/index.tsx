208328be6c7b2df59eee62eac2c3373716730424
Luphia Chang <luphia@cafeca.io>
2024-08-14 21:36:50 +0800

    update last modified date

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Head from 'next/head';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { ILocale } from '@/interfaces/locale';
+import { getLastModifiedDate } from '@/lib/utils/common_backend';
 import Header from '@/components/common/header';
 import HomePageBody from '@/components/home_page/home_page_body';
 import Footer from '@/components/common/footer';
 
-const HomePage = () => {
-  return (
+type HomePageProps = {
+  lastModifiedDate: string;
+};
+
+const HomePage: React.FC<HomePageProps> = ({ lastModifiedDate }) => {
+  const jsx = (
     <>
       <Head>
         <link rel="icon" href="/logo/isuncoin_logo.svg" />
@@ -21,16 +26,22 @@ const HomePage = () => {
       <HomePageBody />
 
       {/* Info:(20240808 - Julian) Footer */}
-      <Footer />
+      <Footer lastModifiedDate={lastModifiedDate} />
     </>
   );
+  return jsx;
 };
 
-const getStaticPropsFunction = async ({ locale }: ILocale) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['common'])),
-  },
-});
+const getStaticPropsFunction = async ({ locale }: ILocale) => {
+  const lastModifiedDate = await getLastModifiedDate();
+  const staticProps = {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      lastModifiedDate,
+    },
+  };
+  return staticProps;
+};
 
 export const getStaticProps = getStaticPropsFunction;
 
